0f8326e17179ea290154a995cace5561706d77f9
filyafilkin <johnnywolf159@gmail.com>
2022-09-06 02:35:24 +0300

    add js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,33 @@
 let close = document.querySelector(".burger__close");
+let hero = document.querySelector(".hero__container");
 let burger = document.querySelector(".burger__open"),
   menu = document.querySelector(".burger");
 
 burger.addEventListener("click", function (e) {
-  menu.classList.add("active");
-  close.classList.add("active");
-  burger.classList.remove("active");
+  menu.classList.toggle("active");
+  close.classList.toggle("active");
+  burger.classList.toggle("active");
+  hero.classList.toggle("hero__active");
 });
 
 close.addEventListener("click", function (e) {
-  menu.classList.remove("active");
-  close.classList.remove("active");
-  burger.classList.add("active");
+  menu.classList.toggle("active");
+  close.classList.toggle("active");
+  burger.classList.toggle("active");
+  hero.classList.toggle("hero__active");
 });
+
+let radio = document.querySelectorAll("shop");
+let submitButton = document.querySelector(".shop__btn");
+
+function go() {
+  var radios = document.getElementById("radios").getElementsByTagName("INPUT"),
+    radio;
+  for (var i = 0, L = radios.length; i < L; i++) {
+    radio = radios[i];
+    if (radio.checked) {
+      location.href = radio.value;
+      break;
+    }
+  }
+}
